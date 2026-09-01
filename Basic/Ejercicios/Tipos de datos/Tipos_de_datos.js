a2bdd916114760a043d2d6afd0f16ba7a1497d61
Carlos <marincarlosandres42@gmail.com>
2026-08-31 20:24:32 -0500

    Ejercitando el cerebrito

---
+++ Basic/Ejercicios/Tipos de datos/Tipos_de_datos.js
@@ -0,0 +1,102 @@
+// Ejercicios (basicos)
+
+// 1. Escribe un comentario en una línea
+
+    //Hola, este es un comentario de una sola linea
+  
+// 2. Escribe un comentario en varias líneas
+
+   /* Hola, este es un comentario de varias líneas */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+    //Cadena de texto
+    let nombre= "Andres"
+    console.log(nombre)
+
+    //Numero
+    let number=25
+    let number2= 25.5
+
+    let resultado= number + number2
+    console.log(resultado)
+
+    //Booleano
+    let verdadero=!false;
+    console.log(verdadero)
+
+    //Undefined
+    let NoDefinido=undefined
+    console.log(NoDefinido)
+
+    //Null
+    let nulo=null
+    console.log(nulo)
+
+
+// 4. Imprime por consola el valor de todas las variables
+
+    //Hecha en el paso 3
+
+// 5. Imprime por consola el tipo de todas las variables
+
+    console.log(typeof nombre)
+    console.log(typeof number)
+    console.log(typeof verdadero)
+    console.log(typeof NoDefinido)
+    console.log(typeof nulo)
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+    nombre= "Carlos"
+    console.log(nombre)
+
+    number= 30
+    number2= 30.5
+    resultado= number + number2
+    console.log(resultado)
+
+    verdadero=!true
+    console.log(verdadero)
+
+    NoDefinido= "Nuevo valor"
+    console.log(NoDefinido)
+
+    nulo= "Nuevo valor"
+    console.log(nulo)
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+    nombre= 25
+    console.log(nombre)
+
+    number= "Hola"
+    console.log(number)
+
+    verdadero= 30.5
+    console.log(verdadero)
+
+    NoDefinido= 30
+    console.log(NoDefinido)
+
+    nulo= 30.5
+    console.log(nulo)
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+    const nombreConstante= "Andres"
+    const numberConstante=25
+    const verdaderoConstante=!false
+    const NoDefinidoConstante=undefined
+    const nuloConstante=null
+
+// 9. A continuación, modifica los valores de las constantes
+
+    const nombreConstante2= "CarlosJuan"
+     const numberConstante2=25
+    const verdaderoConstante2=!false
+    const NoDefinidoConstante2=undefined
+    const nuloConstante2=null
+
+
+
